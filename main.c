7ccffc8c39a2dbc7d6baf856528f763337d52ece
Ukuma012 <Ukuma012@gmail.com>
2023-09-21 08:37:48 +0900

    exit

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "utils.h"
 
 int main(int argc, char* argv[]) {
@@ -7,6 +8,14 @@ int main(int argc, char* argv[]) {
     while(1) {
         prompt();
         read_cmd(buffer);
+
+        if(strcmp(buffer->buffer, "exit") == 0) {
+            printf("%s\n", "Bye");
+            break;
+        }
+        
         printf("%s\n", buffer->buffer);
     }
+
+    return 0;
 }
